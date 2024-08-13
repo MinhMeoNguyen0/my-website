5e759e637cff0fc1980c23976a34630554401b08
Minh Meo <dminhnguyen161@gmail.com>
2024-08-13 14:43:17 -0700

    alsmost

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import About from '@/components/about'
 import Intro from '@/components/intro'
+import Projects from '@/components/projects'
+import SectionDivider from '@/components/section-divider'
+import Skills from '@/components/skills'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Intro />
-
+      <SectionDivider />
+      <About/>
+      <SectionDivider />
+      <Projects/>
+      <SectionDivider />
+      <Skills/>
+    </main>
   )
 }
